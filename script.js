849a84261ed9dfb1917712b8273012fe5c785256
Kyle Berner <kyleberner14@yahoo.com>
2020-03-22 16:15:04 -0700

    html framework

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
-//Check local storage for cities searched
-//Last city viewed should still be displayed
-//Previous cities should be viewed on the previous cities area
 // have the browser connect with the weather API
+
+var apiKey = "98c8d6587120aac4b2819891e6ac8be1"
+
+// Dump JSON for current weather in Philadelphia
+
+// Dump JSON of UV index for Philadelphia
+
+// Dump JSON for five day forecast for Philadelphia
+
+// Log url for current weather icon to the console
+
+// Log URLs for 5-day forecast to the console
+
+
+
 // when a user enters a city
 // create a div to enter the elements of the city into
 // create variables for city name, and other elements that the API gives you
