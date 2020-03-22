df2e709be1ed7a6b3d0095069a05d4d06b2b8ff6
Kyle Berner <kyleberner14@yahoo.com>
2020-03-22 11:05:43 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+//Check local storage for cities searched
+//Last city viewed should still be displayed
+//Previous cities should be viewed on the previous cities area
+// have the browser connect with the weather API
+// when a user enters a city
+// create a div to enter the elements of the city into
+// create variables for city name, and other elements that the API gives you
+// place the variables into the div
