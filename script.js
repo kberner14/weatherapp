fe77fe305bcca0ea9eb4c4b8f672ec71f608e81a
Kyle Berner <kyleberner14@yahoo.com>
2020-03-22 20:05:13 -0700

    finally got the UV query to work

--- script.js
+++ script.js
@@ -4,28 +4,50 @@ var search = "Philadelphia"
 var currentWeatherView = $("#current-weather-view")
 var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=Philadelphia&appid=98c8d6587120aac4b2819891e6ac8be1"
 
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function(response) {
-    currentWeatherView.text(JSON.stringify(response, null, 2));
-    console.log(JSON.stringify(response, null, 2))
-    
-    var currentWeatherData = {
-        cityName: response.name,
-        date: response.dt,
-        icon: response.weather[0].icon,
-        humidityPercent: response.main.humidity,
-        windMS: response.wind.speed
-    }
-    console.log(currentWeatherData)
-});
+
+
 // Dump JSON for current weather in Philadelphia
 // Icon, temperature, humidity, wind speed, and UV index
+function yourWeather() {
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
+        // currentWeatherView.text(JSON.stringify(response, null, 2));
+        // console.log(JSON.stringify(response, null, 2))
+        
+        var currentWeatherData = {
+            cityName: response.name,
+            date: response.dt,
+            icon: response.weather[0].icon,
+            humidityPercent: response.main.humidity,
+            windMS: response.wind.speed,
+            coordinates: response.coord
+        }
+        // console.log(currentWeatherData)
+    });
+}
+yourWeather();
 
-  
-  // Having trouble finding UV index. Maybe ask for help on this
-  
+// UV is a separate API. Need Lat/Long from initial query as coordinates for new query
+function yourUV() {
+    var uvLocation = { lon: -75.16, lat: 39.95 };
+    var queryURL = "http://api.openweathermap.org/data/2.5/uvi?appid=98c8d6587120aac4b2819891e6ac8be1&lat="
+    + uvLocation.lat
+    + "&lon="
+    + uvLocation.lon;
+
+    
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
+    currentWeatherView.text(JSON.stringify(response, null, 2));
+    var cityUV = response.value;
+    console.log("UV Index: " + cityUV );
+    });
+}
+yourUV();
   // Dump JSON for five day forecast for Philadelphia
   
   // Log url for current weather icon to the console
