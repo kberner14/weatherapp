4a2713e9781c11f0a3fd8faa50576277925d3106
Kyle Berner <kyleberner14@yahoo.com>
2020-03-22 20:23:03 -0700

    replaced some static figures with their variable counterparts

--- script.js
+++ script.js
@@ -2,7 +2,8 @@
 var apiKey = "98c8d6587120aac4b2819891e6ac8be1"
 var search = "Philadelphia"
 var currentWeatherView = $("#current-weather-view")
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=Philadelphia&appid=98c8d6587120aac4b2819891e6ac8be1"
+var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=Philadelphia&appid="+apiKey;
+console.log(queryURL)
 
 
 
@@ -32,7 +33,9 @@ yourWeather();
 // UV is a separate API. Need Lat/Long from initial query as coordinates for new query
 function yourUV() {
     var uvLocation = { lon: -75.16, lat: 39.95 };
-    var queryURL = "http://api.openweathermap.org/data/2.5/uvi?appid=98c8d6587120aac4b2819891e6ac8be1&lat="
+    var queryURL = "https://api.openweathermap.org/data/2.5/uvi?appid="
+    + apiKey
+    + "&lat="
     + uvLocation.lat
     + "&lon="
     + uvLocation.lon;
