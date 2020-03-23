4943ed3d6440b8e3a12dddc84bc350f448b49d29
Kyle Berner <kyleberner14@yahoo.com>
2020-03-22 18:55:20 -0700

    initial data dump using Philly as reference

--- script.js
+++ script.js
@@ -1,17 +1,36 @@
 // have the browser connect with the weather API
-
 var apiKey = "98c8d6587120aac4b2819891e6ac8be1"
+var search = "Philadelphia"
+var currentWeatherView = $("#current-weather-view")
+var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=Philadelphia&appid=98c8d6587120aac4b2819891e6ac8be1"
 
+$.ajax({
+    url: queryURL,
+    method: "GET"
+}).then(function(response) {
+    currentWeatherView.text(JSON.stringify(response, null, 2));
+    console.log(JSON.stringify(response, null, 2))
+    
+    var currentWeatherData = {
+        cityName: response.name,
+        date: response.dt,
+        icon: response.weather[0].icon,
+        humidityPercent: response.main.humidity,
+        windMS: response.wind.speed
+    }
+    console.log(currentWeatherData)
+});
 // Dump JSON for current weather in Philadelphia
+// Icon, temperature, humidity, wind speed, and UV index
 
-// Dump JSON of UV index for Philadelphia
-
-// Dump JSON for five day forecast for Philadelphia
-
-// Log url for current weather icon to the console
-
-// Log URLs for 5-day forecast to the console
-
+  
+  // Having trouble finding UV index. Maybe ask for help on this
+  
+  // Dump JSON for five day forecast for Philadelphia
+  
+  // Log url for current weather icon to the console
+  
+  // Log URLs for 5-day forecast to the console
 
 
 // when a user enters a city
